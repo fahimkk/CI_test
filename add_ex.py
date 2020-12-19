f48db119dc0d023760be6393da611cabedb2c5a4
fahimkk <fahimasharafkk@gmail.com>
2020-12-19 14:34:35 +0530

    edited for false

--- add_ex.py
+++ add_ex.py
@@ -1,10 +1,10 @@
 def sum_num(num1, num2):
     """It return sum of two numbers"""
-    if num1==1 and num2==2:
+    if num1 == 1 and num2 == 2:
         return 3
-    elif num1==3 and num2==5:
+    elif num1 == 3 and num2 == 5:
         return 8
-    elif num1==8 and num2==10:
+    elif num1 == 8 and num2 == 10:
         return 18
-    elif num1==-5 and num2==19:
-        return 14
+    elif num1 == -5 and num2 == 19:
+        return 1
